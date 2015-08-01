a39da941c6d7756fc0f72542ea8003a4b73e357c
Carlos Peña <charly_13_eduard@hotmail.com>
2015-08-01 13:56:49 -0500

    Modificación 3

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Expirar sesión tras dos minutos inactivos o 120000 milisegundos
+app.use(function (req, res, next) {
+  var tiempo = 120000;
+  req.session.cookie.expires = new Date(Date.now() + tiempo);
+  next();
+});
+
 // Helpers dinamicos:
 app.use(function(req, res, next) {
     
